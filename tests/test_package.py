52dcdb0d650995cc0867d94b2641adc2ec011c47
Michał Szczygieł <michalszc05@gmail.com>
2025-11-24 21:07:53 +0100

    test: introduce lint/type-check errors to verify GitHub Actions

--- tests/test_package.py
+++ tests/test_package.py
@@ -3,14 +3,14 @@ Dummy tests for torch_batteries package.
 """
 
 
-def test_dummy_pass() -> None:
+def test_dummy_pass():
     """Dummy test that always passes."""
     assert True
 
 
 def test_dummy_math() -> None:
     """Dummy test with simple math."""
-    assert 1 + 1 == 2
+    assert 1 + 1 != 2
 
 
 def test_dummy_string() -> None:
