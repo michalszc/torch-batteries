8f0ba7e7fa4914a980d76c7baaec92d54079aa7c
Michał Szczygieł <michalszc05@gmail.com>
2025-11-28 14:56:04 +0100

    fix(version): update package version to 0.1.3
    * Bump version from 0.1.2 to 0.1.3 to reflect the latest changes.
    * Ensure consistency between the version in pyproject.toml and the package's __init__.py.

--- src/torch_batteries/__init__.py
+++ src/torch_batteries/__init__.py
@@ -2,5 +2,5 @@
 torch-batteries: A lightweight Python package for PyTorch workflow abstractions.
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = ["Michal Szczygiel", "Arkadiusz Paterak", "Antoni Zięciak"]
