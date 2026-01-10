a419072aaecfdfe27d12c3b9518364c144370718
Tosiekdev <antonizieciak@gmail.com>
2026-01-10 10:26:01 +0100

    Higher version, innit?

--- src/torch_batteries/__init__.py
+++ src/torch_batteries/__init__.py
@@ -2,7 +2,7 @@
 torch-batteries: A lightweight Python package for PyTorch workflow abstractions.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __author__ = ["Michal Szczygiel", "Arkadiusz Paterak", "Antoni Zięciak"]
 
 # Import main components
